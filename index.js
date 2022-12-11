95625e5040a23e8520d7a87dfd3a4902bb49ebc8
EMMANUEL <dammylola299@gmail.com>
2022-12-11 12:26:04 +0100

    Designed UI for the project

---
+++ index.js
@@ -0,0 +1,28 @@
+// local reviews data
+const reviews = [
+    {
+        id: 1,
+        name: 'Emmanuel Damilola',
+        profession: 'software engineer',
+        img: 'profile-pic',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur id suscipit nihil voluptates fuga maxime maiores voluptatem quo veritatis, ipsum labore, sed provident culpa veniam odio deserunt consequatur ab voluptatibus dolorum cumque iure. Nulla fugit reiciendis quia, beatae incidunt qui rerum sint eos placeat libero saepe voluptas, inventore expedita voluptatibus!'
+    },
+
+    {
+        id: 2,
+        name: 'Nikola tesla',
+        profession: 'Electrical engineer',
+        img: 'profile-pic',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur id suscipit nihil voluptates fuga maxime maiores voluptatem quo veritatis, ipsum labore, sed provident culpa veniam odio deserunt consequatur ab voluptatibus dolorum cumque iure. Nulla fugit reiciendis quia, beatae incidunt qui rerum sint eos placeat libero saepe voluptas, inventore expedita voluptatibus!'
+    },
+
+    {
+        id: 3,
+        name: 'Tony stark',
+        profession: 'Scientist',
+        img: 'profile-pic',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur id suscipit nihil voluptates fuga maxime maiores voluptatem quo veritatis, ipsum labore, sed provident culpa veniam odio deserunt consequatur ab voluptatibus dolorum cumque iure. Nulla fugit reiciendis quia, beatae incidunt qui rerum sint eos placeat libero saepe voluptas, inventore expedita voluptatibus!'
+    }
+];
+
+console.log(reviews);
